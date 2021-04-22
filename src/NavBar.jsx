4d348f85e3d09697b0e986e81b2a5440db236ce6
alinauman219 <alinauman219@gmail.com>
2021-04-22 16:18:24 +0000

    boostrap installed and UI layout

---
+++ src/NavBar.jsx
@@ -0,0 +1,10 @@
+function NavBar() {
+  return (
+    <nav class="navbar navbar-light bg-light">
+        <span class="navbar-brand mb-0 h1" ><a href="/">Socka Players</a></span>
+        <a class="float-right" href="/add" title="Add a New Player">Add a Player</a>
+    </nav>
+  );
+}
+
+export default NavBar;
